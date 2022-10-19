520352bbb13ce4a55943286eeb8e23c6190fe00b
Nigredo499 <nigredo.499@gmail.com>
2022-10-19 12:12:02 +0300

    Add functions that generate a filename in the required format.

--- payment_order.py
+++ payment_order.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 def collect_pay_info(file: str) -> list:
-    # Парсит платёжного поручения, выгруженного из 1С.
+    # Парсит платёжные поручения, выгруженные из 1С.
 
     # Сопоставление ключей 1С и XML
     translators = {
@@ -42,6 +42,33 @@ def collect_pay_info(file: str) -> list:
     return docpack
 
 
+def file_name(n: int) -> str:
+    # Возвращает имя файла для экспорта в требуемом формате
+    code = 'TSE_0401060_D07_'   # Код платежного поручения.
+    org = '123Я4567'            # Код организации (учетный номер по Сводному реестру).
+    return code + org + _dm_convert(datetime.now()) + _convert36(n) + '.XML'
+
+
+def _dm_convert(date) -> str:
+    # Конвертирует день в формат для имени файла: 1–9, A–V, и месяц: 1–9, A–С.
+    days = '0123456789ABCDEFGHIJKLMNOPQRSTUV'
+    months = '0123456789ABC'
+    return days[date.day] + months[date.month]
+
+
+def _convert36(n) -> str:
+    #  Конвертирует число в 36-ричный формат, возвращая строку из 6-ти символов.
+    #  (соответствие требованиям именования файлов для импорта)
+    base = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    result = ''
+    while n:
+        n, y = divmod(n, 36)
+        result = base[y] + result
+    if len(result) < 6:
+        result = '0' * (6 - len(result)) + result
+    return result
+
+
 if __name__ == '__main__':
     f = '/path/to/1C_file.txt'
-    pay_oerders = collect_pay_info(f)
+    pay_orders = collect_pay_info(f)
